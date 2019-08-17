4a0e1d8df518314364e821b84c7ce780ece9758a
shiweing <shiweing1998@gmail.com>
2019-08-17 23:36:48 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -10,16 +11,35 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList();
         // Greet
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        String input = scanner.next();
+        String input = scanner.nextLine();
         // Echo
         while (!input.equals("bye")) {
-            System.out.println(input);
-            input = scanner.next();
+            if(input.equals("list")) {
+                // List
+                displayList(list);
+            }
+            else {
+                // Add
+                list.add(input);
+                System.out.println("added: " + input);
+            }
+
+            input = scanner.nextLine();
         }
         // Exit
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void displayList(ArrayList<String> list) {
+        int index = 1;
+
+        for (String task : list) {
+            System.out.printf("%d. %s%n", index, task);
+            index++;
+        }
     }
 }
